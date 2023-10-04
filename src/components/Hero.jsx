fb2130438d78e6b047a881f9664bcc72ef4f2e36
marrwan <abdulbasitdamilola6@gmail.com>
2023-10-04 18:46:25 +0100

    solving errors on hero aspect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Mexbg from '../assets/mexbg.png';
 
 function Hero() {
   return (
-    <div className="bg-gradient-to-r from-[#EEF0FF] to-[#F7ECFF] gap-x-10 grid w-screen lg:grid-cols-2 xs:grid-rows-2 lg:p-20 xs:px-20 xs:pt-10 h-screen">
+    <div className="bg-gradient-to-r from-[#EEF0FF] to-[#F7ECFF] gap-x-10 grid w-screen lg:grid-cols-2 xs:grid-rows-2 lg:p-20 xs:px-20 xs:pt-10 lg:h-screen">
       <div className='lg:my-auto'>
         <div className="mb-5">
         <p className="font-medium">MEX.AI</p>
@@ -14,7 +14,7 @@ function Hero() {
         <p className="mt-10 font-medium leading-relaxed xs:-ml-10 lg:ml-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus illum soluta quidem ut molestias esse corrupti velit tempore vero deleniti. A, nam nobis et delectus autem ducimus tempore nihil cumque magnam porro! Animi, alias ab. </p>
         <button className="xs:-ml-10 lg:ml-0 bg-[#6D49FD] text-white rounded-md p-2 mt-5"> Learn More </button>
       </div>
-      <img className="lg:-mt-20 w-full md:-mt-[11rem] sm:-mt-[7rem] xs:mt-[7rem]" src={Hero_logo} />
+      <img className="lg:-mt-20 w-full md:-mt-[9rem] sm:-mt-[7rem] xs:mt-[.1rem]" src={Hero_logo} />
     </div>
   );
 }
